c5573dfb30cd106e7674d5d39412c83423f8a763
Ram Kumar <ramk8055@gmail.com>
2022-02-02 21:54:39 +0530

    Initial commit with components and services

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddInputComponent } from './add-input/add-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddInputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
