43cdb28afebdbce83d0bff90ec8b721a3732bb2d
Ram Kumar <ramk8055@gmail.com>
2022-02-04 15:41:40 +0530

    Addig Chart components and basic OC layout

--- src/app/add-input/add-input.component.ts
+++ src/app/add-input/add-input.component.ts
@@ -12,12 +12,13 @@ export class AddInputComponent implements OnInit {
   margin: string = '';
   
   
-  expiry: string = ''
-  lot: string = '1'
-  strikeprice: number = 18000
-  pe_ce : string ='PE'
-  buy_sell: string = 'sell'
+  expiry: string[] = []
+  lot: string[] = []
+  strikeprice: number[] = []
+  pe_ce : string[] = []
+  buy_sell: string[] = []
 
+  total: number = 0
 
   constructor(private apiService: ApiServicesService) { }
 
@@ -41,43 +42,102 @@ export class AddInputComponent implements OnInit {
 
 
 
-  getExpiry(event: any){
-    this.expiry = event.target.value
+  getExpiry(event: any, index: number){
+    this.expiry[index] = event.target.value
 
   }
-  getStrike(event: any){
-    this.strikeprice = event.target.value
+  getStrike(event: any, index: number){
+    this.strikeprice[index] = event.target.value
   }
   
-  reduceStrike(){
-    this.strikeprice -=50
+  reduceStrike(index: number){
+    this.strikeprice[index] -=50
     this.getApiData()
 
   }
 
-  increaseStrike(){
-    this.strikeprice = 50 +parseInt(this.strikeprice+"") 
+  increaseStrike(index: number){
+    this.strikeprice[index] = 50 +parseInt(this.strikeprice+"") 
     this.getApiData()
 
   }
 
-  getPeCe(event: any){
-    this.pe_ce = event.target.value
+  getPeCe(event: any, index: number){
+    this.pe_ce[index] = event.target.value
   }
 
-  getLot(event: any){
-    this.lot = event.target.value
+  getLot(event: any, index: number){
+    this.lot[index] = event.target.value
   }
 
-  getBuySell(event: any){
-    this.buy_sell = event.target.value
+  getBuySell(event: any, index: number){
+    this.buy_sell[index] = event.target.value
   }
 
   getApiData(){
     var body = this.pe_ce+ " " + this.strikeprice + " " + this.lot;
-    this.apiService.getApiData(body).then((data)=>{ this.margin = JSON.stringify(data)});
+    // this.apiService.getApiData(body)
+    //   .then((data)=>
+    //   { 
+    //     this.margin = JSON.stringify(data)
+    //   });
 
     // console.log(this.pe_ce+ " " + this.strikeprice + " " + this.lot)
   }
 
+
+  createRange(){
+    var items: number[] = [];
+    for(var i = 1; i <= this.total; i++){
+      items.push(i);
+    }
+    return items;
+
+    // return ['1','2','3'];
+  }
+
+  addPosition(){
+    if(this.total==0 || (this.expiry[this.total-1] 
+      && this.strikeprice[this.total-1]
+      && this.lot[this.total-1]
+      && this.pe_ce[this.total-1]
+      && this.buy_sell[this.total-1])){
+        this.total++
+      }
+      else{
+        console.log("Fill all values")
+      }
+  }
+
+  isSelectedRow(index: number){
+
+  }
+
+  deleteRow(index: number){
+
+  }
+  addTest(){
+    console.log(this.expiry)
+    this.expiry.push("exp")
+    this.strikeprice.push(100)
+    this.lot.push("2")
+    this.pe_ce.push("CE")
+    this.buy_sell.push("sell")
+    this.total++
+  }
+  optionChain(){
+    var items: number[] = [];
+    for(var i = 1; i <= 10; i++){
+      items.push(i*100);
+    }
+    return items;
+  }
+
+  positionAdd(index:number,pe_ce:string,buy_sell:string){
+    console.log(index)
+    console.log(pe_ce)
+    console.log(buy_sell)
+    //add new input
+    // trigger calc
+  }
 }
